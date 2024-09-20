d3a178e75a108c507ac367745c06860a0bb682ba
Shubham Gupta <114250610+shub7800@users.noreply.github.com>
2024-09-20 14:49:03 +0000

    container config done

---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict" // treat all js code in newer version
+
+//alert("hello")
+//alert(3+3)// we are using note js not browser
+
+console.log(3
+    +
+    3);// code readablity should be high 
+
+let name="shubham"
+let age=18
+let isLoggedIn=false
+
+//number => 2 to power 53
+// bigint usde for big number like sensex
+// string  => ""
+// bollean => true/ false 
+//null => stand alone vaLUE (empty value )
+// UNDEFINED =>
+//symbol =>it use for uniqueness 
+
+//object
+
+console.log(typeof"shubham"); // it is use to define the data type 
+console.log(typeof null);// object 
+console.log(typeof undefined);// undefind
+
+
