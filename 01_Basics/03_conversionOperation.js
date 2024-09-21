2751324fc84660faac78c7a76fe5b4db265f3293
Shubham Gupta <114250610+shub7800@users.noreply.github.com>
2024-09-21 07:15:17 +0000

    container config done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,72 @@
+let score ="26abc"
+console.log(typeof score);//string 
+
+let valueInNumber= Number(score )// this type fixed the datatype with repect to given data type 
+console.log(typeof valueInNumber);// Number , 
+console.log(valueInNumber);// it give NaN  
+ 
+//"33" =>33
+//"33abc"  => NaN not a number 
+// true =>1 ; false=0
+
+
+let  isLoggedIn1=1
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+console.log(typeof booleanIsLoggedIn1);
+console.log(booleanIsLoggedIn1);
+
+let  isLoggedIn="shubham"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 =>  true;0=>false
+//"" => false
+//"shubham"=>true
+
+let someNumber=33;
+
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+let check=null
+
+let StringCheck=String(check)
+console.log(StringCheck);
+console.log(typeof StringCheck);
+
+//***************************OPERATION******************************/
+
+let value=3
+let negValue=-value
+console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);//power
+console.log(2%2);//remainder
+
+let str1="hello"
+let str2="shubham"
+let str3=str1+ str2
+console.log(str3);
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+console.log(+true);
+console.log("");
+console.log(+"");
+
+
+
+
+
+
+
+ 
+
+
+
