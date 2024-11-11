e116af455aef0ff99af673f182990fb55ac40275
Shubham Gupta <114250610+shub7800@users.noreply.github.com>
2024-11-11 06:23:34 +0000

    container config done

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,14 @@
+//********************they are not good to use  */
+console.log("2">1);//true
+// in js it automatcally canvert string into a number 
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+
+console.log(undefined==0);//false
+ 
+//********************************************** */
+/* strict check === 
+        it check  value as well as data type */
+
+console.log("2"===2);//false 
